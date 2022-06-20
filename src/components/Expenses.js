fbb4a04be7ff585b9c85b27fe8eacbb752821ad7
anguscg <angus.cg@hotmail.com>
2022-06-20 20:38:17 +1000

    updated to filter to display expenses in that given year

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -13,14 +13,17 @@ function Expenses (props) {
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
-        
-    }
+    };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
+                {filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
             </Card>
         </div>
     )
